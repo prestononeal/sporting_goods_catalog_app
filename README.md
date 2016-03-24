eb722743e19e5712c219abb6b6c73d9c0ece5911
Preston O'Neal <prestononeal@gmail.com>
2016-03-23 22:00:43 -0700

    Add main landing page template

--- README.md
+++ README.md
@@ -3,4 +3,14 @@
 A web application that provides a list of items within a variety of categories.
 A user registration and authentication system is provided so that users can post, edit, and delete their own items and categories.
 
-Required: This app must be run in a Vagrant-configured VirtualBox VM. The Vagrant settings were applied from this project: https://github.com/udacity/fullstack-nanodegree-vm
+The HTML/CSS layout was borrowed heavily from [https://css-tricks.com/super-simple-two-column-layout/](https://css-tricks.com/super-simple-two-column-layout/)
+
+Required: This app must be run in a Vagrant-configured VirtualBox VM. The Vagrant settings were applied from this project: [https://github.com/udacity/fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm)
+
+To run this project:
+
+1. Navigate to the root folder containing [project.py](project.py)
+2. Run `python database_setup.py` to set up the database
+3. Run `python lots_of_items.py` to populate the database with some initial data
+4. Run `python project.py` to run the web app. Naviate to [http://localhost:8000](http://localhost:8000)
+5.  in your browser.
--- project.py
+++ project.py
@@ -1,6 +1,24 @@
 # The main program for the Item Catalog Web App
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+
+# Temporary database items. TODO: remove
+categories = [{'name': 'Soccer', 'id': 1},
+              {'name': 'Baseball', 'id': 2},
+              {'name': 'Volleyball', 'id': 3}]
+
+
+items = [{'id': 1, 'category': {'name': 'Soccer', 'id': 1},
+          'item': 'Ball'},
+         {'id': 2, 'category': {'name': 'Baseball', 'id': 2},
+          'item': 'Bat'},
+         {'id': 3, 'category': {'name': 'Volleyball', 'id': 3},
+          'item': 'Net'},
+         {'id': 4, 'category': {'name': 'Baseball', 'id': 2},
+          'item': 'Mitt'},
+         {'id': 5, 'category': {'name': 'Volleyball', 'id': 3},
+          'item': 'Shoes'}]
 
 
 @app.route('/login/')
@@ -16,7 +34,10 @@ def logout():
 @app.route('/')
 @app.route('/catalog/')
 def catalog_main():
-    return 'The main catalog page'
+    # Get the three latest items
+    newest_items = items[-3:]
+    return render_template('catalog.html', categories=categories,
+                           items=newest_items)
 
 
 @app.route('/catalog/category/<int:category_id>/')
@@ -39,23 +60,23 @@ def category_delete(category_id):
     return 'Deleting category {}'.format(category_id)
 
 
-@app.route('/catalog/category/<int:category_id/item/<int:item_id>/')
-def item_main(category_id, item_id):
+@app.route('/catalog/item/<int:item_id>/')
+def item_main(item_id):
     return 'Showing description of item {}'.format(item_id)
 
 
-@app.route('/catalog/category/<int:category_id/item/add/')
-def item_add(category_id):
+@app.route('/catalog/item/add/')
+def item_add():
     return 'Add a new item'
 
 
-@app.route('/catalog/category/<int:category_id/item/<int:item_id>/edit/')
-def item_edit(category_id, item_id):
+@app.route('/catalog/item/<int:item_id>/edit/')
+def item_edit(item_id):
     return 'Editing item {}'.format(item_id)
 
 
-@app.route('/catalog/category/<int:category_id/item/<int:item_id>/delete/')
-def item_delete(category_id, item_id):
+@app.route('/catalog/item/<int:item_id>/delete/')
+def item_delete(item_id):
     return 'Deleting item {}'.format(item_id)
 
 
