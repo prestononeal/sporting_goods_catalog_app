5ecc0c4de5178f49c6fe5614838c1e3f78ed1c3e
Preston O'Neal <prestononeal@gmail.com>
2016-03-24 21:32:56 -0700

    Add Google login/logout

--- project.py
+++ project.py
@@ -1,8 +1,21 @@
 '''
 The main program for the Item Catalog Web App
 '''
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, \
+    make_response
+from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+from oauth2client.client import OAuth2Credentials
+import httplib2
+import random
+import string
+import json
+import requests
 app = Flask(__name__)
+
+G_CLIENT_ID = json.loads(open('g_client_secrets.json', 'r').read(
+    ))['web']['client_id']
 
 
 # Temporary database items. TODO: remove
@@ -25,7 +38,111 @@ items = [{'id': 1, 'category': {'name': 'Soccer', 'id': 1},
 
 @app.route('/login/')
 def login():
-    return 'The login screen for the app'
+    state = ''.join(random.choice(string.ascii_uppercase +
+                                  string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', state=state)
+
+
+@app.route('/gconnect/', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state '
+                                            'parameter'),
+                                 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        # Exchange the auth code for a credentials object
+        oauth_flow = flow_from_clientsecrets(
+            'g_client_secrets.json',
+            scope='https://www.googleapis.com/auth/userinfo.email')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to get credentials from Google'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Check that the access token is valid
+    access_token = credentials.access_token
+    url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?' \
+          'access_token={}'.format(access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there are errors in the access token, abort
+    if result.get('error') is not None:
+        response = make_response(
+            json.dumps(result.get('error'), 500))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # The token is valid. Make sure it's for the right user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps('Tokens user ID does not match given user id.'),
+            401
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Verify that the access token is valid for this app
+    if result['issued_to'] != G_CLIENT_ID:
+        response = make_response(
+            json.dumps('Tokens client ID does not apps.'),
+            401
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Check to see if user is already logged in
+    stored_credentials = login_session.get('credentials')
+    if stored_credentials is not None:
+        # Get a credentials object from the stored json.
+        # The OAuth2Credentials class is not serializable
+        stored_credentials = OAuth2Credentials.from_json(stored_credentials)
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+            json.dumps('Current user is already connected'),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Store the access token in the session for later use
+    login_session['credentials'] = credentials.to_json()
+    login_session['gplus_id'] = gplus_id
+    # Get user info
+    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    params = {
+        'access_token': credentials.access_token,
+        'alt': 'json'
+    }
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+    login_session['provider'] = 'google'
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    # Create a User object in the database for this user if one does not exist
+    user_id = get_user_id(login_session['email'])
+    if not user_id:
+        user_id = create_user(login_session)
+    login_session['user_id'] = user_id
+    # Print the welcome message to the user
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += 'You are user number {}'.format(user_id)
+    output += '<img src="'
+    output += login_session['picture']
+    output += '" style="width: 300px; height: 300px;'
+    output += 'border-radius: 150px;'
+    output += '-webkit-border-radius: 150px;'
+    output += '-moz-border-radius: 150px;">'
+    flash('You are now logged in as {}'.format(login_session['username']))
+    return output
 
 
 @app.route('/logout/')
@@ -82,6 +199,26 @@ def item_delete(item_id):
     return 'Deleting item {}'.format(item_id)
 
 
+def get_user_id(email):
+    # Gets the user ID from the user's email address
+    try:
+        return 1  # TODO: DB check
+    except:
+        return None
+
+
+def create_user(login_session):
+    # Creates a new user in the database and returns its ID
+    new_user = User(name=login_session['username'],
+                    email=login_session['email'],
+                    picture=login_session['picture'])
+    session.add(new_user)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = 'super_secret_key'
     app.run(host='0.0.0.0', port=8000)
